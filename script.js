8f578c607b8ed727dddaed4ed40c48ba9fd26ded
Danijel Gomola <180227226+dgomola314@users.noreply.github.com>
2024-09-18 23:59:56 +0200

    Add functionality to game UI

--- script.js
+++ script.js
@@ -1,65 +1,97 @@
-function getComputerChoice() {
-    const randomNumber = Math.round(Math.random() * 3);
-
-    if (randomNumber === 1) {
-        return "rock";
-    } else if (randomNumber === 2) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
-}
-
-function getHumanChoice() {
-    const choice = prompt("Rock, paper or scissors?");
-
-    return choice;
-}
-
 function playGame() {
+    // Necessary variables initialized on at the top of playGame function
+    const humanResult = document.querySelector("#human-result");
+    const computerResult = document.querySelector("#computer-result");
+    const message = document.querySelector("#message");
+    const finalMessage = document.querySelector("#final-message");
+    const buttons = document.querySelectorAll(".button");
     let humanScore = 0;
     let computerScore = 0;
     let round = 0;
 
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === "rock" && computerChoice === "paper") {
-            computerScore++;
-            return `${humanChoice.toUpperCase()} ${humanScore}:${computerScore} ${computerChoice.toUpperCase()}`;
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            humanScore++;
-            return `${humanChoice.toUpperCase()} ${humanScore}:${computerScore} ${computerChoice.toUpperCase()}`;
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            humanScore++;
-            return `${humanChoice.toUpperCase()} ${humanScore}:${computerScore} ${computerChoice.toUpperCase()}`;
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            computerScore++;
-            return `${humanChoice.toUpperCase()} ${humanScore}:${computerScore} ${computerChoice.toUpperCase()}`;
-        } else if (humanChoice === "scissors" && computerChoice === "rock") {
-            computerScore++;
-            return `${humanChoice.toUpperCase()} ${humanScore}:${computerScore} ${computerChoice.toUpperCase()}`;
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            humanScore++;
-            return `${humanChoice.toUpperCase()} ${humanScore}:${computerScore} ${computerChoice.toUpperCase()}`;
+    // getComputerChoice generates random choice by computer
+    function getComputerChoice() {
+        const randomNumber = Math.round(Math.random() * 3);
+    
+        if (randomNumber === 1) {
+            return "rock";
+        } else if (randomNumber === 2) {
+            return "paper";
         } else {
-            return `${humanChoice.toUpperCase()} ${humanScore}:${computerScore} ${computerChoice.toUpperCase()}`;
+            return "scissors";
         }
     }
 
-    while (round < 5) {
-        const humanSelection = getHumanChoice().toLowerCase();
+    // playRound compares both choices and determines who should get a point
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === "rock" && computerChoice === "paper") {
+            computerScore++;
+            humanResult.textContent = `${humanScore}`;
+            computerResult.textContent = `${computerScore}`;
+        } else if (humanChoice === "rock" && computerChoice === "scissors") {
+            humanScore++;
+            humanResult.textContent = `${humanScore}`;
+            computerResult.textContent = `${computerScore}`;
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            humanScore++;
+            humanResult.textContent = `${humanScore}`;
+            computerResult.textContent = `${computerScore}`;
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
+            computerScore++;
+            humanResult.textContent = `${humanScore}`;
+            computerResult.textContent = `${computerScore}`;
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
+            computerScore++;
+            humanResult.textContent = `${humanScore}`;
+            computerResult.textContent = `${computerScore}`;
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            humanScore++;
+            humanResult.textContent = `${humanScore}`;
+            computerResult.textContent = `${computerScore}`;
+        } else {
+            humanResult.textContent = `${humanScore}`;
+            computerResult.textContent = `${computerScore}`;
+        }
+    }
+
+    // getHumanChoice allows the user to pick their choice by clicking on the one of the three adequate buttons
+    function getHumanChoice(event) {
+        const humanSelection = event.target.id;
         const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        message.textContent = `Computer chose ${computerSelection}`;
+        document.querySelector("#reset").disabled = true;
 
-        console.log(playRound(humanSelection, computerSelection));
         round++;
+        if (round === 5) {
+            endGame();
+        }
     }
 
-    if (humanScore > computerScore) {
-        return "You won!";
-    } else if (humanScore < computerScore) {
-        return "Computer won! Better luck next time.";
-    } else {
-        return "It's tie!";
+    // endGame determines the winner after 5 played rounds 
+    function endGame() {
+        if (humanScore > computerScore) {
+            finalMessage.textContent += "You won!";
+        } else if (humanScore < computerScore) {
+            finalMessage.textContent += "Computer won!";
+        } else {
+            finalMessage.textContent += "It's tie!";
+        }
+
+        buttons.forEach(button => {
+            button.disabled = true;
+        });
+
+        document.querySelector("#reset").disabled = false;
     }
+
+    buttons.forEach(button => {
+        button.addEventListener("click", getHumanChoice);
+    });
+
+    document.querySelector("#reset").onclick = function() {
+        location.reload();
+    };
 }
 
-console.log(playGame());
+playGame();
